d44c55117a213eadb76959365238de950fe6ec39
Dener Niels <ksin_niels@outlook.com>
2024-02-13 10:10:24 +0000

    Adicionar arquivos de projeto.

---
+++ Repository/Contracts/ICustomerRepository.cs
@@ -0,0 +1,9 @@
+﻿using DependencyStore.Models;
+
+namespace DependencyStore.Repository.Contracts
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer?> GetByIdAsync(string customerId);
+    }
+}
