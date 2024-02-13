d57e1676c18328ca7b03b1c47ac7cd93f753ae7a
Dener Niels <ksin_niels@outlook.com>
2024-02-13 11:57:58 +0000

    Resolvendo as dependências

--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
+using DependencyStore;
+using DependencyStore.Models;
+using DependencyStore.Repository;
+using DependencyStore.Repository.Contracts;
+using DependencyStore.Services;
+using DependencyStore.Services.Contracts;
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<Configuration>();
+builder.Services.AddScoped<SqlConnection>(x
+    => new SqlConnection("CONN_STRING"));
+builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
+builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
+builder.Services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
